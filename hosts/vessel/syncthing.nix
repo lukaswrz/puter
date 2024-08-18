21c5e76082e2f0733bc000d7870647b7fa68614f
Lukas Wurzinger <lukas@wrz.one>
2024-08-18 20:58:05 +0200

    fix

--- hosts/vessel/syncthing.nix
+++ hosts/vessel/syncthing.nix
@@ -1,7 +1,12 @@
-{
+let
+  guiPort = 8384;
+in {
   services.syncthing = {
     enable = true;
     systemService = true;
     openDefaultPorts = true;
+    guiAddress = "0.0.0.0:${guiPort}";
   };
+
+  networking.firewall.allowedTCPPorts = [guiPort];
 }
