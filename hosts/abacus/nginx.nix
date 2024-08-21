5de57a417e0bab834575be6633032481cab31606
Lukas Wurzinger <lukas@wrz.one>
2024-08-21 18:59:24 +0200

    fixes

--- hosts/abacus/nginx.nix
+++ hosts/abacus/nginx.nix
@@ -5,12 +5,11 @@
   services.nginx = {
     enable = true;
 
-    recommendedBrotliSettings = true;
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
-    # recommendedZstdSettings = true;
+
     commonHttpConfig = ''
       error_log stderr;
       access_log /var/log/nginx/access.log;
