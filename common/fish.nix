eeeef710a6165415fa58d4dffe0d5f34ac698bd5
Lukas Wurzinger <lukas@wrz.one>
2024-12-01 05:33:14 +0100

    whatever

--- common/fish.nix
+++ common/fish.nix
@@ -2,14 +2,4 @@
   programs.fish.enable = true;
 
   users.defaultUserShell = pkgs.fish;
-
-  # nixpkgs.overlays = [
-  #   (final: prev: {
-  #     fish = prev.fish.overrideAttrs (_: {
-  #       postInstall = ''
-  #         rm $out/share/applications/fish.desktop
-  #       '';
-  #     });
-  #   })
-  # ];
 }
