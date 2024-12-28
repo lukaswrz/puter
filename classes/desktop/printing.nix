0136475c721226c55b65b8fab71673761b1b20e2
Lukas Wurzinger <lukas@wrz.one>
2024-12-29 00:32:35 +0100

    puter

--- classes/desktop/printing.nix
+++ classes/desktop/printing.nix
@@ -1,19 +1,6 @@
-{pkgs, ...}: {
+{
   services.printing = {
     enable = true;
     webInterface = true;
-    cups-pdf.enable = true;
-    drivers = [
-      pkgs.gutenprint
-      pkgs.gutenprintBin
-      pkgs.hplip
-      pkgs.hplipWithPlugin
-      pkgs.postscript-lexmark
-      pkgs.samsung-unified-linux-driver
-      pkgs.splix
-      pkgs.brlaser
-      pkgs.brgenml1lpr
-      pkgs.cnijfilter2
-    ];
   };
 }
