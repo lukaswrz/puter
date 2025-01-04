123e68747982efc9937bb06257ec9bae2c5479b2
Lukas Wurzinger <lukas@wrz.one>
2025-01-05 00:36:22 +0100

    fix

--- packages/musicomp/default.nix
+++ packages/musicomp/default.nix
@@ -6,14 +6,21 @@
 python3Packages.buildPythonApplication {
   pname = "musicomp";
   version = "0.1.0";
+
   src = ./.;
+
   pyproject = true;
+
   doCheck = false;
+
   build-system = [python3Packages.hatchling];
+
   makeWrapperArgs = [
     "--prefix"
     "PATH"
     ":"
     (lib.makeBinPath [opusTools])
   ];
+
+  meta.mainProgram = "musicomp";
 }
