e3f4355ccc3719854a9645010cf9a658d2090a5f
Lukas Wurzinger <lukas@wrz.one>
2025-01-05 01:41:06 +0100

    fix

--- hosts/server/vessel/blocky.nix
+++ hosts/server/vessel/blocky.nix
@@ -1,23 +1,26 @@
 let
   upstream = "https://one.one.one.one/dns-query";
 in {
-  services.blocky = {
-    enable = true;
-    settings = {
-      ports.dns = 53;
-      upstreams.groups.default = [upstream];
-      bootstrapDns = {
-        inherit upstream;
-        ips = ["1.1.1.1" "1.0.0.1"];
-      };
-      blocking = {
-        denylists.ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
-        clientGroupsBlock.default = ["ads"];
-      };
-      caching = {
-        minTime = "5m";
-        maxTime = "30m";
-        prefetching = true;
+  services = {
+    resolved.extraConfig = "DNSStubListener=no";
+    blocky = {
+      enable = true;
+      settings = {
+        ports.dns = 53;
+        upstreams.groups.default = [upstream];
+        bootstrapDns = {
+          inherit upstream;
+          ips = ["1.1.1.1" "1.0.0.1"];
+        };
+        blocking = {
+          denylists.ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+          clientGroupsBlock.default = ["ads"];
+        };
+        caching = {
+          minTime = "5m";
+          maxTime = "30m";
+          prefetching = true;
+        };
       };
     };
   };
