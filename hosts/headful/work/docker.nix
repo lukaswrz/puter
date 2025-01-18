0139527b3b8c1e43bdc8030fe27c62739864237c
Lukas Wurzinger <lukas@wrz.one>
2025-01-18 19:13:14 +0100

    docker

--- hosts/headful/work/docker.nix
+++ hosts/headful/work/docker.nix
@@ -1,5 +1,13 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   virtualisation.docker.enable = true;
 
   users.groups.docker.members = config.users.normalUsers;
+
+  environment.systemPackages = [
+    pkgs.docker-compose
+  ];
 }
