5e59462491fe5cb2f88aa87623c7fff4b597f4d2
Lukas Wurzinger <lukas@wrz.one>
2025-01-19 00:59:05 +0100

    weirdness

--- hosts/headful/work/envfs.nix
+++
@@ -1,3 +0,0 @@
-{
-  services.envfs.enable = true;
-}
--- hosts/headful/work/phps.nix
+++ hosts/headful/work/phps.nix
@@ -15,6 +15,8 @@
     "php84"
   ];
 
+  selectedPhp = "php84";
+
   extraConfig = ''
     memory_limit = -1
 
@@ -34,15 +36,54 @@
         inherit extraConfig;
       }
   );
+
+  prefix = "/var/lib/phps";
+  /*
+  phpHomeLink = ".local/bin/php";
+
+  usephp = pkgs.writeShellApplication {
+    name = "usephp";
+    text = ''
+      if (( $# != 1 )); then
+        echo incorrect number of arguments >&2
+        exit 1
+      fi
+      ln --symbolic --force -- ${lib.escapeShellArg prefix}/"$1"/bin/php ~/${lib.escapeShellArg phpHomeLink}
+    '';
+  };
+
+  composer = pkgs.writeShellApplication {
+    name = "composer";
+    text = ''
+      php=~/${lib.escapeShellArg phpHomeLink}
+      if [[ ! -x $php ]]; then
+        echo php has not been linked >&2
+        exit 1
+      fi
+      exec -- "$php" ${pkgs.phpPackages.composer}/bin/.composer-wrapped
+    '';
+  };
+  */
 in {
   nix.settings = {
     substituters = ["https://fossar.cachix.org/"];
     trusted-public-keys = ["fossar.cachix.org-1:Zv6FuqIboeHPWQS7ysLCJ7UT7xExb4OE8c4LyGb5AsE="];
   };
 
-  systemd.tmpfiles.settings.php =
+  systemd.tmpfiles.settings =
     builtins.mapAttrs (name: drv: {
-      "/var/lib/phps/${name}"."L+".argument = drv.outPath;
+      "${prefix}/${name}"."L+".argument = drv.outPath;
     })
     phps;
+
+  environment.systemPackages = [
+    phps.${selectedPhp}.packages.composer
+  ];
+
+  /*
+  environment.systemPackages = [
+    usephp
+    composer
+  ];
+  */
 }
