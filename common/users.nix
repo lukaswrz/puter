5a904c8507a0adfb7fbb4407ca9c8453da6eba3a
Lukas Wurzinger <lukas@wrz.one>
2025-03-09 17:03:39 +0100

    pluh 🗣

--- common/users.nix
+++ common/users.nix
@@ -13,17 +13,14 @@ in {
     mainUser = "lukas";
 
     users = {
-      root = {
-        hashedPassword = "!";
-        # TODO define this more granularly
-        openssh.authorizedKeys.keys = [];
-      };
+      root.hashedPassword = "!";
       ${mainUser} = {
         description = "Lukas Wurzinger";
         uid = 1000;
         isNormalUser = true;
         hashedPasswordFile = config.age.secrets."user-${mainUser}".path;
         openssh.authorizedKeys.keys = builtins.attrValues config.pubkeys.users;
+        extraGroups = ["wheel"];
       };
     };
   };
