f2ed82ad3f81730773d2c1158cee62115e5c708d
Lukas Wurzinger <lukas@wrz.one>
2025-03-30 16:20:43 +0000

    stuff

--- common/syncthing.nix
+++ common/syncthing.nix
@@ -1,9 +1,11 @@
-{
+{config, ...}: let
+  inherit (config.networking) hostname;
+in {
   services.syncthing = {
     enable = true;
     systemService = true;
     openDefaultPorts = true;
-    guiAddress = "0.0.0.0:4000";
+    guiAddress = "${hostname}.tailnet.helveticanonstandard.net:4000";
     overrideDevices = false;
     overrideFolders = false;
   };
