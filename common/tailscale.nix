08189a69e0f9646ad3d74e4d1e75d6d4ac9b7a8c
Lukas Wurzinger <lukas@wrz.one>
2025-03-30 15:58:29 +0000

    stuff

--- common/tailscale.nix
+++ common/tailscale.nix
@@ -10,5 +10,8 @@
     ];
     # Required to connect to Tailscale exit nodes
     checkReversePath = "loose";
+    interfaces.${config.services.tailscale.interfaceName} = {
+      allowedTCPPorts = [4000];
+    };
   };
 }
