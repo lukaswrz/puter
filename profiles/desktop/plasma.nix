0300b0ed3a1f9bf0e5c6aab274714d1502802e66
Lukas Wurzinger <lukas@wrz.one>
2025-08-21 07:43:07 +0200

    fixes

--- profiles/desktop/plasma.nix
+++ profiles/desktop/plasma.nix
@@ -10,16 +10,10 @@ in
 {
   config = lib.mkIf cfg.enable {
     services = {
-      desktopManager = {
-        cosmic.enable = lib.mkForce false;
-        plasma6.enable = true;
-      };
-      displayManager = {
-        cosmic-greeter.enable = lib.mkForce false;
-        sddm = {
-          enable = true;
-          wayland.enable = true;
-        };
+      desktopManager.plasma6.enable = true;
+      displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
       };
     };
 
