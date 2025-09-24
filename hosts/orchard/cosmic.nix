157b537d129f37d53a6f277ad9795c446e69c8af
Lukas Wurzinger <lukas@wrz.one>
2025-09-24 22:59:41 +0200

    cleanup

---
+++ hosts/orchard/cosmic.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.profiles.desktop;
+in
+{
+  config = lib.mkIf cfg.enable {
+    services = {
+      desktopManager.cosmic.enable = true;
+      displayManager.cosmic-greeter.enable = true;
+    };
+
+    environment.cosmic.excludePackages = [
+      pkgs.cosmic-edit
+      pkgs.cosmic-player
+    ];
+  };
+}
