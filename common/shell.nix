22759353169b3872d267a91b96a53a6d2eed71af
Lukas Wurzinger <lukas@wrz.one>
2025-09-24 23:45:41 +0200

    shell cleanup

--- common/shell.nix
+++ common/shell.nix
@@ -1,22 +1,4 @@
-{ config, ... }:
+{ pkgs, ... }:
 {
-  programs = {
-    fish.enable = true;
-
-    bash.interactiveShellInit = ''
-      shopt -s autocd globstar nullglob extglob checkwinsize
-    '';
-
-    starship = {
-      enable = true;
-      interactiveOnly = true;
-      settings.format = "$all";
-    };
-  };
-
-  users.defaultUserShell = config.programs.fish.package;
-
-  environment.systemPackages = [
-    config.programs.starship.package
-  ];
+  users.defaultUserShell = pkgs.nushell;
 }
