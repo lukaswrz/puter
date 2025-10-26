3259b9056769825115f745c6cbe8737b486d4db0
Lukas Wurzinger <lukas@wrz.one>
2025-10-26 14:34:58 +0100

    asdf

--- pubkeys.nix
+++ pubkeys.nix
@@ -4,7 +4,6 @@
 
   hosts = {
     abacus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHoUgClpkOlBEffQOb9KkVn970RwnIhU0OiVr7P2WVzg";
-    flamingo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIInV+UpCZhoTwgkgnCzCPEu3TD5b5mu6tagRslljrFJ/";
     orchard = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBnG8uYvtENzaXsfS2be1MT7AP9Ph8CM5IR7xBfVxZ3J";
     vessel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKkYcOb1JPNLTJtob1TcuC08cH9P2APAhLR26RYd573d";
     kaleidoscope = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEMuiAjx1H7ZhHdztAJH+QIVpjpewcdFEwZcOuIucysJ";
