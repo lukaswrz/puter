760a208c1364901a8be6b405976cd2d73fb2387f
Lukas Wurzinger <lukas@wrz.one>
2025-10-26 13:45:02 +0100

    asdf

--- hosts/vessel/rsync.nix
+++ hosts/vessel/rsync.nix
@@ -2,40 +2,46 @@
 {
   services.rsync = {
     enable = true;
-    jobs = {
-      vault = {
-        sources = [ "/srv/vault/" ];
-        destination = "/srv/sync";
-        inhibit = [ "sleep" ];
-        settings = {
-          archive = true;
-          delete = true;
-          mkpath = true;
-          exclude = "lost+found/";
+    jobs =
+      let
+        ssh = lib.getExe pkgs.openssh;
+      in
+      {
+        vault = {
+          sources = [ "/srv/vault/" ];
+          destination = "/srv/sync";
+          inhibit = [ "sleep" ];
+          settings = {
+            archive = true;
+            delete = true;
+            mkpath = true;
+            exclude = "lost+found/";
+          };
         };
-      };
 
-      roms = {
-        sources = [ "/srv/vault/roms/" ];
-        destination = "insomniac@kaleidoscope:~/Roms";
-        inhibit = [ "sleep" ];
-        settings = {
-          archive = true;
-          delete = true;
-          mkpath = true;
+        roms = {
+          sources = [ "/srv/vault/roms/" ];
+          destination = "insomniac@kaleidoscope:~/Roms";
+          inhibit = [ "sleep" ];
+          settings = {
+            archive = true;
+            delete = true;
+            mkpath = true;
+            rsh = ssh;
+          };
         };
-      };
 
-      movies = {
-        sources = [ "/srv/vault/movies/" ];
-        destination = "insomniac@kaleidoscope:~/Videos/Movies";
-        inhibit = [ "sleep" ];
-        settings = {
-          archive = true;
-          delete = true;
-          mkpath = true;
+        movies = {
+          sources = [ "/srv/vault/movies/" ];
+          destination = "insomniac@kaleidoscope:~/Videos/Movies";
+          inhibit = [ "sleep" ];
+          settings = {
+            archive = true;
+            delete = true;
+            mkpath = true;
+            rsh = ssh;
+          };
         };
       };
-    };
   };
 }
