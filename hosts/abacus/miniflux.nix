9192ae23709a08f829a2ff17d79fc36ee8cda46c
Lukas Wurzinger <lukas@wrz.one>
2025-11-01 18:02:29 +0100

    stuff

--- hosts/abacus/miniflux.nix
+++ hosts/abacus/miniflux.nix
@@ -13,7 +13,7 @@
     adminCredentialsFile = config.age.secrets.miniflux.path;
     config = {
       LISTEN_ADDR = "${config.networking.hostName}.tailent.helveticanonstandard.net:6010";
-      BASE_URL = "https://${virtualHostName}";
+      BASE_URL = "http://${config.networking.hostName}.tailent.helveticanonstandard.net";
       CREATE_ADMIN = 1;
       WEBAUTHN = 1;
     };
