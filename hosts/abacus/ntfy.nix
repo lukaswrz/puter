8046f670d1a48c4f6caa179c8a61c22de5e9031a
Lukas Wurzinger <lukas@wrz.one>
2025-11-26 00:28:45 +0100

    idk

--- hosts/abacus/ntfy.nix
+++ hosts/abacus/ntfy.nix
@@ -3,22 +3,24 @@ let
   virtualHostName = "poke.helveticanonstandard.net";
 in
 {
-  services.ntfy-sh = {
-    enable = true;
-    settings = {
-      listen-http = "localhost:8050";
-      auth-default-access = "deny-all";
-      base-url = "https://${virtualHostName}";
-    };
-  };
+  # services.ntfy-sh = {
+  #   enable = true;
+  #   settings = {
+  #     listen-http = "localhost:8050";
+  #     auth-default-access = "deny-all";
+  #     base-url = "https://${virtualHostName}";
+  #     require-login = true;
+  #     web-root = "disable";
+  #   };
+  # };
 
-  services.nginx.virtualHosts.${virtualHostName} = {
-    enableACME = true;
-    forceSSL = true;
+  # services.nginx.virtualHosts.${virtualHostName} = {
+  #   enableACME = true;
+  #   forceSSL = true;
 
-    locations."/" = {
-      proxyPass = "http://${config.services.ntfy-sh.settings.listen-http}";
-      proxyWebsockets = true;
-    };
-  };
+  #   locations."/" = {
+  #     proxyPass = "http://${config.services.ntfy-sh.settings.listen-http}";
+  #     proxyWebsockets = true;
+  #   };
+  # };
 }
