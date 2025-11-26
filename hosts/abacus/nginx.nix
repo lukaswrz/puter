99bd4d4fc63d65ad18dd08efec055984a2f844b7
Lukas Wurzinger <lukas@wrz.one>
2025-11-26 23:26:18 +0100

    rmminiflux

--- hosts/abacus/nginx.nix
+++ hosts/abacus/nginx.nix
@@ -6,28 +6,18 @@
     recommendedBrotliSettings = true;
     recommendedOptimisation = true;
     recommendedProxySettings = true;
-    # recommendedTlsSettings = true;
+    recommendedTlsSettings = true;
 
     commonHttpConfig = ''
       error_log stderr;
       access_log /var/log/nginx/access.log;
     '';
 
-    virtualHosts =
-      let
-        matchAll = ''~.*'';
-        matchWww = ''~^www\.(?<domain>.+)$'';
-      in
-      {
-        # Redirect anything that doesn't match any server name to networking.domain
-        #${matchAll} = {
-        #  default = true;
-        #  rejectSSL = true;
+    virtualHosts."~.*" = {
+      default = true;
+      # rejectSSL = true;
 
-        #  globalRedirect = config.networking.domain;
-        #};
-        # Redirect www to non-www
-        # ${matchWww}.globalRedirect = "$domain";
-      };
+      globalRedirect = config.networking.domain;
+    };
   };
 }
